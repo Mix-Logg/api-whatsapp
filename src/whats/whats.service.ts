e5c3a8f10dcfa9ba7c4460156ea117a04d13cddc
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2024-07-31 11:35:38 -0300

    FIX: debug

--- src/whats/whats.service.ts
+++ src/whats/whats.service.ts
@@ -92,6 +92,7 @@ export class WhatsService {
       };
     } catch (err) {
       console.error('Failed to send message', err);
+      process.exit(1)
       return {
         status: 500,
         message: 'Server internal error'
@@ -112,6 +113,7 @@ export class WhatsService {
       };
     } catch (err) {
       console.error('Failed to send message', err);
+      process.exit(1)
       return {
         status: 500,
         message: 'Server internal error'
