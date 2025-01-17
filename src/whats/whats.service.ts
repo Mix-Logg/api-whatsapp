17a3cdb2ad62b5c99dd2f03c7e913b032daf3308
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2025-01-17 14:03:45 -0300

    TEST: testing fix

--- src/whats/whats.service.ts
+++ src/whats/whats.service.ts
@@ -15,6 +15,10 @@ export class WhatsService {
   onModuleInit() {
     this.client = new Client({
       authStrategy: new LocalAuth(),
+      webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+      },
       puppeteer: {
         executablePath: '/snap/bin/chromium',
         headless: true,  
