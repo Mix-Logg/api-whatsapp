25582e14f49afcfa2715a9733046aab4faad39eb
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2025-01-17 14:08:04 -0300

    FEAT: path

--- src/whats/whats.service.ts
+++ src/whats/whats.service.ts
@@ -35,7 +35,7 @@ export class WhatsService {
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
       },
       puppeteer: {
-        // executablePath: '/snap/bin/chromium',
+        executablePath: '/snap/bin/chromium',
         headless: true,  
         args: [
           '--no-sandbox',
